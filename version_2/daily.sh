867e6f3286bf1b95b79e8d7af73261078199ac67
Calvin <cheuk.lo@uoit.net>
2016-04-12 17:12:09 -0400

    DONE

--- version_2/daily.sh
+++ version_2/daily.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+# CSCI 3060/ SOFE 3980 Course Project Daily Script
+# Group: Blissfully Elephant
+# Member: Calvin Lo, Matthew Mccormick
+# Usage: ./daily.sh <directory of test cases> <MasterAccounts> <BankAccount> <MergedFile> <New BankAccount> <new MasterAccounts> <Interactive (0 or 1)>
+# eg. 
+# ./daily.sh Day0 Daily_MasterAccounts.txt Daily_BankAccount.txt Daily_MergedTransactionFile.txt Daily_New_MasterAccounts.txt Daily_New_BankAccount.txt 0
 
-# change BankAccount_Daily and MasterAccounts_Daily to BankAccount and MasterAccounts
-mv Daily_MasterAccounts.txt MasterAccounts.txt 
-mv Daily_BankAccount.txt BankAccount.txt
+if [ $# != 7 ]; then
+    echo "Usage: ./daily.sh <directory of test cases> <MasterAccounts> <BankAccount> <MergedFile> <New BankAccount> <new MasterAccounts> <Interactive (0 or 1)>"
+    exit
+fi
+
+# change BankAccount input and MasterAccounts input to BankAccount and MasterAccounts
+mv $2 MasterAccounts.txt 
+mv $3 BankAccount.txt
 
 # run test cases input
-for file in TestCases/*
+for file in $1/*
 do
     cat "$file" | ./FrontEnd/FrontEnd
 done
 
-./FrontEnd/FrontEnd # run interactive input
+if [ $7 = 1 ]; then
+    ./FrontEnd/FrontEnd # run interactive input
+fi
 
 # merge
 truncate -s 0 MergedTransactionFile.txt # clear the contents
@@ -26,14 +39,14 @@ rm TransactionFiles/*
 # run the backend
 java BackEnd.Main MergedTransactionFile.txt
 
-# change BankAccount and MasterAccounts to BankAccount_Daily and MasterAccounts_Daily
-mv MasterAccounts.txt Daily_MasterAccounts.txt 
-mv BankAccount.txt Daily_BankAccount.txt
+# change BankAccount and MasterAccounts to original name
+mv MasterAccounts.txt $2 
+mv BankAccount.txt $3
 
-mv MergedTransactionFile.txt Daily_MergedTransactionFile.txt
-mv new_BankAccounts.txt Daily_New_BankAccount.txt
-mv new_MasterAccounts.txt Daily_New_MasterAccounts.txt
+mv MergedTransactionFile.txt $4
+mv new_MasterAccounts.txt $5
+mv new_BankAccounts.txt $6
 
 # Delete the newline
-truncate -s -1 Daily_New_MasterAccounts.txt Daily_New_BankAccount.txt
+truncate -s -1 $5 $6
 
--- version_2/weekly.sh
+++ version_2/weekly.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
+# CSCI 3060/ SOFE 3980 Course Project Daily Script
+# Group: Blissfully Elephant
+# Member: Calvin Lo, Matthew Mccormick
 
-# change BankAccount_Weekly and MasterAccounts_Weekly to BankAccount and MasterAccounts
-for i in `seq 1 4`
-do
-    mv Weekly_Day$((i-1))_MasterAccounts.txt MasterAccounts.txt 
-    mv Weekly_Day$((i-1))_BankAccount.txt BankAccount.txt
-    for file in Day$i/*
-    do
-        cat "$file" | ./FrontEnd/FrontEnd
-    done
+for i in `seq 1 5`
+do    
+    Cases=Day$i
+    Master=Weekly_Day$((i-1))_MasterAccounts.txt
+    Bank=Weekly_Day$((i-1))_BankAccount.txt
+    Merged=Weekly_Day"$i"_MergedTransactionFile.txt
+    NewMaster=Weekly_Day"$i"_MasterAccounts.txt
+    NewBank=Weekly_Day"$i"_BankAccount.txt
     
-    # merge
-   truncate -s 0 MergedTransactionFile.txt # clear the contents
-   DATE=`date "+%m%d%Y"` # get the current date
-   for file in TransactionFiles/$DATE*
-   do
-        cat "$file" >> MergedTransactionFile.txt
-   done
-
-   rm TransactionFiles/*
-    
-    # run the backend
-   java BackEnd.Main MergedTransactionFile.txt
-    
-    # change BankAccount and MasterAccounts to BankAccount_Weekly and MasterAccounts_Weekly
-    mv MasterAccounts.txt Weekly_Day$((i-1))_MasterAccounts.txt
-    mv BankAccount.txt Weekly_Day$((i-1))_BankAccount.txt
-    
-    mv MergedTransactionFile.txt Weekly_Day"$i"_MergedTransactionFile.txt
-    mv new_BankAccounts.txt Weekly_Day"$i"_BankAccount.txt
-    mv new_MasterAccounts.txt Weekly_Day"$i"_MasterAccounts.txt
-    
-    # Delete the newline
-    truncate -s -1 Weekly_Day"$i"_MasterAccounts.txt Weekly_Day"$i"_BankAccount.txt
+    ./daily.sh $Cases $Master $Bank $Merged $NewMaster $NewBank 0
     
 done
+ 
